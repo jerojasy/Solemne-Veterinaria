d7a0c2213b0360e21aa35ded1a02d896c2242fb2
Jaime Rojas <jaimee.rojas@hotmail.com>
2019-09-14 10:28:12 -0300

    se agrega js

---
+++ js.js
@@ -0,0 +1,20 @@
+Jquery('document').ready(function($){
+    var subir = $('.back-to-top');
+
+    subir.click(function(e){
+        e.preventDefault();
+
+        $('html, body').animate({scrollTop: 0}, 500);
+    });
+
+    subir.hide();
+
+    $(window).scroll(function(){
+        
+        if( $(this).scrollTop() > 200 ){
+            subir.fadeIn();
+        }else{
+            subir.fadeOut();
+        }
+    });
+});
